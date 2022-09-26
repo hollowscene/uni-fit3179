e349526cd44d0758aa77ffdaf9729faa3261bd30
Andrew Ting <andrewt.7077@gmail.com>
2022-09-26 12:10:11 +1000

    Attempt Melbourne map

---
+++ assignment2/vega_lite_map.js
@@ -0,0 +1,4 @@
+var vg_1 = "melbourne_trees.vg.json";
+vegaEmbed("#treemap", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
