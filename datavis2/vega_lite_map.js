83ca8e3c581e0fa5dfcc2440ed9f7cce7de49c65
Andrew Ting <andrewt.7077@gmail.com>
2022-10-14 12:14:26 +1100

    Income grouped bar chart

--- datavis2/vega_lite_map.js
+++ datavis2/vega_lite_map.js
@@ -12,3 +12,8 @@ var vg_3 = "carbon_worst_emitters.vg.json";
 vegaEmbed("#topkchart", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "carbon_income_bar.vg.json";
+vegaEmbed("#groupedbarchart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
